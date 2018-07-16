b349c948a22d04e6fc6c5700007ec92af931943e
z8985561 <356839046@qq.com>
2018-07-16 23:17:01 +0800

    起飞页 v-1.1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 /**
- * Created by 电 on 2018/7/7.
+ * Created by 电 on 2018/7/12.
  */
+$(function(){
+ 	var $uList = $("#evaluate2 .container .row");
+ 	var index = 0;
+ 	var $spans = $(".indicators span");
+ 	$("#next").click(function(){
+
+ 		index < $spans.length - 1 ? index++ : index = 0;
+ 		uTarget(index);
+ 		$spans.eq(index).addClass("on").siblings().removeClass("on");
+ 	})
+ 	$("#prev").click(function(){
+ 		index > 0 ? index-- : index = $spans.length - 1;
+ 		uTarget(index);
+ 		$spans.eq(index).addClass("on").siblings().removeClass("on");
+ 	})
+ 	$spans.click(function(){
+ 		$(this).addClass("on").siblings().removeClass("on");
+ 		index = $(this).index();
+ 		uTarget(index);
+ 	})
+ 	var uTarget = function (a){
+ 		$uList.animate({left:-1190 * a +'px'});
+ 	}
+});
